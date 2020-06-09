36a65f9966775431b01e048f83b63a27274caabf
Justin Grimes <jgrimes@relias.com>
2020-06-09 17:17:30 -0400

    Add Home, About, and Courses pages. Add navigation

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './components/App.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+render(
+  <Router>
+    <App/>
+  </Router>,
+  document.getElementById('app')
+);
